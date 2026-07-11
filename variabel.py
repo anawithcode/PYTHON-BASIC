785f184bf77e268bb2962e2cff6de110c5b0af02
Shiva Andriyana <anacode101@gmail.com>
2026-07-12 00:38:25 +0800

    variabel python

---
+++ variabel.py
@@ -0,0 +1,10 @@
+# variabel dinamis python ini bisa diubah bahkan jika tipe data nya berbeda
+nama = "Ana"
+umur = 19
+tinggi = 157.5
+
+# Aturan Penamaan Variabel
+# - Tidak boleh dimulai dengan angka
+# - Tidak boleh pakai tanda minus (-)
+# - Tidak boleh pakai keyword Python
+# - Tidak boleh ada spasi
